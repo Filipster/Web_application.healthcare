312623e2016206586e9d748b4b6b279648e58be9
Marcus Gonçalves <nvrrox@gmail.com>
2020-10-16 00:57:44 -0300

    changing visions from the initial project

--- src/main_page.py
+++ src/main_page.py
@@ -2,89 +2,107 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
 
 def body(df):
 
     st.title('DATAFLOW - PRAS analysis')
 
-    st.header('Cases per Doctor Speciality')
-    _build_hbar_chart(df, ['PROF_ESP_ATE'])    
+    st.header('Statistics')
+    st.markdown('Total of registers in file: **{}**'.format(len(df)))
+    st.markdown('% Women: **{:.1f}%**'.format(df.PCTE_SEXO.value_counts(normalize=True)['Mujer'] * 100))
+    st.markdown('% Men: **{:.1f}%**'.format(df.PCTE_SEXO.value_counts(normalize=True)['Hombre'] * 100))
 
-    st.subheader('Every chart below will be filtered by selected speciality')
-    speciality = st.selectbox(label='Choose a speciality', options=df['PROF_ESP_ATE'].unique())
+    st.header('HeatMap? (Manter? O que fazer?)')
+    _df = df[['ENT_NOM_LAT', 'ENT_NOM_LONG', 'ENT_DES_PARR']].rename({'ENT_NOM_LAT': 'lat', 'ENT_NOM_LONG': 'lon'}, axis=1)
+    _df = _df.groupby(['lat', 'lon']).agg({'ENT_DES_PARR': 'count'}).reset_index()
+    st.map(_df)
+    
+    st.header('Cases per Month')
+    _df = df.groupby(['ATEMED_MONTH']).agg({'ENT_DES_PARR': 'count'}).reset_index().sort_values(by='ATEMED_MONTH')
+    st.markdown('Month with most register: **{}** with **{:.1f}%** of the total appointments'.format(_df.loc[np.argmax(_df.ENT_DES_PARR)][0], (_df.loc[np.argmax(_df.ENT_DES_PARR)][1] / len(df) * 100)))
 
-    st.header('1 - Doctor Speciality VS Cases per Unit')
-    _build_vbar_chart(df[df['PROF_ESP_ATE'] == speciality], ['ENT_DES_TIP_EST'])
+    labels = list(_df['ATEMED_MONTH'])
+    lbl_values = list(_df['ENT_DES_PARR'])
+    _build_vbar_chart(labels, lbl_values, 0)
 
-    st.header('2 - Doctor Speciality VS Cases per Operative Unit')
-    _build_hbar_chart(df[df['PROF_ESP_ATE'] == speciality], ['ENT_DES_CANT'])
+    # build list of months
+    months = list(df['ATEMED_MONTH'].unique())
+    months.insert(0, 'All Data')
+    month_analyzed = st.selectbox(label='Choose a Month to filter further charts', options=months)
 
-    st.header('3 - Pacients Ethnicities')
-    _build_hbar_chart(df[df['PROF_ESP_ATE'] == speciality], ['PCTE_AUTID_ETN'])
+    # Priority Group Treatment
+    st.header('{} - Priority Appointments'.format(month_analyzed))
+    if month_analyzed == 'All Data':
+        filter_1 = (df[(df.PCTE_GRP_PRI_QTD > 0)])
+        st.markdown('% Priority Group Appointments: **{} - {:.1f}%**'.format(len(filter_1), (len(filter_1) / len(df) * 100)))
+    else:
+        filter_1 = (df[(df.ATEMED_MONTH == month_analyzed) & (df.PCTE_GRP_PRI_QTD > 0)])
+        st.markdown('% Priority Group Appointments: **{} - {:.1f}%**'.format(len(filter_1), (len(filter_1) / len(df[(df.ATEMED_MONTH == month_analyzed)]) * 100)))
 
-    st.header('4 - Priority Groups Distribution')
-    _build_pie_chart(df[df['PROF_ESP_ATE'] == speciality])
+    _ = ['PCTE_GRP_PRI_Embarazadas', 'PCTE_GRP_PRI_Discapacidad', 'PCTE_GRP_PRI_Assedio_Sexual_Trabajo',
+        'PCTE_GRP_PRI_Violencia_Psicologica', 'PCTE_GRP_PRI_Violencia_Sexual', 'PCTE_GRP_PRI_Violencia_Fisica',
+        'PCTE_GRP_PRI_Maltrato_Infantil', 'PCTE_GRP_PRI_Enfermidades_Catastroficas', 'PCTE_GRP_PRI_Desastres_Naturales',
+        'PCTE_GRP_PRI_Desastres_Antropogenicos', 'PCTE_GRP_PRI_Penitenciarios']
+    tt = filter_1[_].sum().transpose()
+    tmp = pd.DataFrame(tt, columns=['t1']).reset_index().sort_values(by='t1', ascending=True)
+
+    labels = list(tmp['index'])
+    lbl_values = list(tmp['t1'])
+
+    _build_hbar_chart(labels, lbl_values, 0)
+    st.markdown('> We have pacients that belongs to more than 1 priority group.')
+    
+    groups = _
+    groups.insert(0, 'All Groups')
+    focus_group = st.selectbox(label='Choose a Priority Group to filter further charts', options=groups)
+
+    # Priority Group Diagnostic
+    st.header('{} - {} - Top 10 Most Frequent Diagnostic'.format(month_analyzed, focus_group))
+
+    if focus_group == 'All Groups':
+        filter_2 = filter_1.copy()
+    else:
+        filter_2 = filter_1[filter_1[focus_group] > 0]
+    
+    _df = filter_2.ATEMED_DES_CIE10.value_counts().head(10)
+
+    labels = list(_df.keys())
+    lbl_values = list(_df.values)
+
+    _build_vbar_chart(labels, lbl_values, 90)
 
 
 
-def _build_vbar_chart(data, fields):
+
+def _build_vbar_chart(labels, lbl_values, angle):
     """Build chart and return its plot."""
     sns.set_palette('rocket')
-    labels = list(data[fields[0]].value_counts().keys())
-    lbl_values = list(data[fields[0]].value_counts().values)
-
-    
+   
     fig, ax = plt.subplots(figsize=(15, 7))
     
     ax.bar(labels, lbl_values)
-    ax.set_xlabel(fields[0])
-    ax.set_ylabel('quantity')
+    plt.xticks(rotation=angle)
 
     for lbl in enumerate(lbl_values):
-        ax.text(lbl[0], lbl[1] + 100, lbl[1])
+        ax.text(lbl[0], lbl[1] + 10, lbl[1])
     
     return st.pyplot(fig)
 
-def _build_hbar_chart(data, fields):
+def _build_hbar_chart(labels, lbl_values, angle):
     """Build chart and return its plot."""
     sns.set_palette('rocket')
-    labels = list(data[fields[0]].value_counts().keys())
-    lbl_values = list(data[fields[0]].value_counts().values)
-
     
     fig, ax = plt.subplots(figsize=(15, 7))
-    
+
     ax.barh(labels, lbl_values)
-    ax.set_ylabel(fields[0])
-    ax.set_xlabel('quantity')
+    plt.xticks(rotation=angle)
 
     for lbl in enumerate(lbl_values):
-        ax.text(lbl[1] + 100, lbl[0], lbl[1])
+        ax.text(lbl[1] + 10, lbl[0], lbl[1])
     
     return st.pyplot(fig)
 
-def _build_pie_chart(df):
-    """Build pie chart from priority groups."""
-    sns.set_palette('rocket')
-
-    cols = ['PCTE_GRP_PRI_Embarazadas',
-       'PCTE_GRP_PRI_Discapacidad', 'PCTE_GRP_PRI_Assedio_Sexual_Trabajo',
-       'PCTE_GRP_PRI_Violencia_Psicologica', 'PCTE_GRP_PRI_Violencia_Sexual',
-       'PCTE_GRP_PRI_Violencia_Fisica', 'PCTE_GRP_PRI_Maltrato_Infantil',
-       'PCTE_GRP_PRI_Enfermidades_Catastroficas',
-       'PCTE_GRP_PRI_Desastres_Naturales',
-       'PCTE_GRP_PRI_Desastres_Antropogenicos', 'PCTE_GRP_PRI_Penitenciarios']
-
-    labels = []
-    lbl_values = []
-    exploding = [0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for col in cols:
-        labels.append(col[13:])
-        lbl_values.append(df[col].sum())
-    
-    fig, ax = plt.subplots(figsize=(12, 12))
-    
-    ax.pie(lbl_values, labels=labels, explode=exploding, startangle=45)
-    plt.legend(loc='lower right')
-    
-    return st.pyplot(fig)
